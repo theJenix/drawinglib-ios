87e9e9051f7707597ae23129b1e6bae88a9c064d
Jesse Rosalia <jesse.rosalia@gatech.edu>
2012-03-03 12:12:34 -0500

    Initial Commit

---
+++ DrawingLibTests/DrawingLibTests.h
@@ -0,0 +1,13 @@
+//
+//  DrawingLibTests.h
+//  DrawingLibTests
+//
+//  Created by Jesse Rosalia on 3/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DrawingLibTests : SenTestCase
+
+@end
